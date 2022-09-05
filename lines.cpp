01d9e735240ffa5f33e6500867e057e1f3b90103
Nikita <nikitaputintsev@gmail.com>
2022-09-05 15:56:42 +0300

    first commit

---
+++ lines.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string line = "Hello World";
+    std::cout << line << "\n" << line.length() << "\n" << line[0] << "\n" << line[line.size()-1] << "\n";
+
+    return 0;
+}
